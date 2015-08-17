31681d8fbbb6ffcbffab38a226c6372e6825933f
alex <Home@192.168.0.75>
2015-08-17 11:16:54 +0300

    Начало дизайна

--- view/main.php
+++ view/main.php
@@ -2,4 +2,37 @@
 //проверка доступа
 defined(ACCESS_VALUE) or die('Access denied');
 
-echo 'main';
+?>
+
+<div id="topMenu">
+    <div id="topMenuFirst">
+        <div id="searchField">
+            <i class="fa fa-search"></i><input value="" type="text" placeholder="Введите название или артикул" class="inp"/><input value="ПОИСК" type="submit" class="btn"/>
+        </div>
+        <a href="<?=PATH?>" id="logo"><img src="<?=VIEW?>images/logo.png"/></a>
+    </div>
+    <div id="topMenuSecond">
+        <div id="topMenuSecondBody">
+            <a href="#" id="basketSmall">
+                <i class="fa fa-shopping-cart"></i> товаров: 0
+            </a>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#"><span>главная</span></a>
+                    </li>
+                    <li>
+                        <a href="#"><span>каталог</span></a>
+                    </li>
+                    <li>
+                        <a href="#"><span>новости и акции</span></a>
+                    </li>
+                    <li>
+                        <a href="#"><span>доставка и оплата</span></a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+        </div>
+    </div>
+</div>
