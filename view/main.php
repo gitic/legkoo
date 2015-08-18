938f8db4fdbc6c84010f284e3910bded7571f615
alex <Home@192.168.0.75>
2015-08-18 09:21:48 +0300

    Дизайн (18.08,09:18)

--- view/main.php
+++ view/main.php
@@ -48,6 +48,47 @@ defined(ACCESS_VALUE) or die('Access denied');
             <img src="<?=VIEW?>images/banner.jpg" alt=""/>
         </div>
         <h3>НОВИНКИ LEGO</h3>
+        <div id="newProduct">
+            <div class="productPreview">
+                <a href="#">
+                    <img src="http://legkoo.com.ua/image/cache/data/Products/Architecture/21022_alt1-1000x1000.jpg" class="big"/>
+                </a>
+                <div class="previewTitle">
+                    <p><span>Арт. 75106</span>LEGO Star Wars</p>
+                    <div class="clear"></div>
+                    <a href="#">Имперский десантный корабль Имперский десантный корабль</a>
+                </div>
+                <div class="previewPrice">
+                    1600 грн
+                </div>
+            </div>
+            <div class="productPreview">
+                <a href="#">
+                    <img src="http://legkoo.com.ua/image/cache/data/Products/Architecture/21022_alt1-1000x1000.jpg" class="big"/>
+                </a>
+                <div class="previewTitle">
+                    <p><span>Арт. 75106</span>LEGO Star Wars</p>
+                    <div class="clear"></div>
+                    <a href="#">Имперский десантный корабль </a>
+                </div>
+                <div class="previewPrice">
+                    1600 грн
+                </div>
+            </div>
+            <div class="productPreview">
+                <a href="#">
+                    <img src="http://legkoo.com.ua/image/cache/data/Products/Architecture/21022_alt1-1000x1000.jpg" class="big"/>
+                </a>
+                <div class="previewTitle">
+                    <p><span>Арт. 75106</span>LEGO Star Wars</p>
+                    <div class="clear"></div>
+                    <a href="#">Имперский</a>
+                </div>
+                <div class="previewPrice">
+                    1600 грн
+                </div>
+            </div>
+        </div>
         <h3>СЕРИИ LEGO</h3>
         <div id="catPreview">            
             <a href="#" class="catPreviewBlock">
@@ -85,12 +126,31 @@ defined(ACCESS_VALUE) or die('Access denied');
 
 <div id="footer">
     <div id="footerBody">
-        <div class="footLogo">
-            <img src="<?=VIEW?>images/logo.png"/>
+        <div class="block">
+            <a href="<?=PATH?>" class="footLogo">
+                <img src="<?=VIEW?>images/logo.png"/>
+            </a>
         </div>
+        <div class="block">
+            <h4>ИНФОРМАЦИЯ</h4>
+            <p><i class="fa fa-hand-o-right"></i> <a href="#">Оплата и доставка</a></p>
+            <p><i class="fa fa-hand-o-right"></i> <a href="#">Дисконтная программа</a></p>
+            <p><i class="fa fa-hand-o-right"></i> <a href="#">Обратная связь</a></p>
+            <p><i class="fa fa-hand-o-right"></i> <a href="#">Условия соглашения</a></p>
+            <p><i class="fa fa-hand-o-right"></i> <a href="#">Гарантия</a></p>
+        </div>
+        <div class="block contacts">
+            <h4>КОНТАКТЫ</h4>
+            <p>Украина, Днепропетровск <br/>
+            просп. Карла Маркса 67-Д </p>
+            <p>ТЦ «Гранд Плаза»</p>
+            <p>Тел: +38(067)567-82-44 <br/>
+            Email: legodnepr@gmail.com</p>
+        </div>
+        <div class="clear"></div>
     </div>
 </div>
 
 <div id="copyright">
-    ds
+    <p>2015 LEGO, логотип LEGO, логотип DUPLO, LEGENDS OF CHIMA, NINJAGO, BIONICLE, MINDSTORMS, MIXELS, ELVES, JUNIORS, PIRATES, CLASSIC, а также minifigure (минифигурка) и кубик LEGO являются торговыми марками корпорации LEGO Group.</p>
 </div>
