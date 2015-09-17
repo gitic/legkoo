890b7b8ae2cebb716eb57ba686a1b774c437ad48
alex <Home@idea-PC>
2015-09-17 12:02:25 +0300

    Fix Саша

--- view/blocks_site/head.php
+++ view/blocks_site/head.php
@@ -56,10 +56,3 @@ defined(ACCESS_VALUE) or die('Access denied');
     });
 
 </script>
-
-<script type="text/javascript" src="//vk.com/js/api/openapi.js?117"></script>
-
-<script type="text/javascript">
-  VK.init({apiId: 5072998, onlyWidgets: true});
-</script>
-
