d0b883b1b4d45536ceb935f0eb277a094bb199b3
alex <Home@idea-PC>
2015-09-22 12:00:44 +0300

    Fix Саша

--- view/product.php
+++ view/product.php
@@ -117,6 +117,20 @@ Product::update(array('views'=>$numView), array('id'=>$id), $conn);
                     <p>Размеры (Д*Ш*В): <strong><?=$product->size?></strong></p>
                     <?php endif;?>
                 </div>
+            <div class="shareBtn">
+        
+<script type="text/javascript">(function(w,doc) {
+if (!w.__utlWdgt ) {
+    w.__utlWdgt = true;
+    var d = doc, s = d.createElement('script'), g = 'getElementsByTagName';
+    s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+    s.src = ('https:' == w.location.protocol ? 'https' : 'http')  + '://w.uptolike.com/widgets/v1/uptolike.js';
+    var h=d[g]('body')[0];
+    h.appendChild(s);
+}})(window,document);
+</script>
+<div data-background-alpha="0.0" data-buttons-color="#FFFFFF" data-counter-background-color="#ffffff" data-share-counter-size="12" data-top-button="false" data-share-counter-type="disable" data-share-style="1" data-mode="share" data-like-text-enable="false" data-mobile-view="false" data-icon-color="#ffffff" data-orientation="horizontal" data-text-color="#000000" data-share-shape="round-rectangle" data-sn-ids="fb.vk.tw.ok.gp.mr.em." data-share-size="30" data-background-color="#ffffff" data-preview-mobile="false" data-mobile-sn-ids="fb.vk.tw.wh.ok.gp." data-pid="1409452" data-counter-background-alpha="1.0" data-following-enable="false" data-exclude-show-more="true" data-selection-enable="false" class="uptolike-buttons" ></div>
+    </div>
                 <div class="block tabs">
                     <ul>
                         <li>Описание</li>
@@ -168,5 +182,6 @@ Product::update(array('views'=>$numView), array('id'=>$id), $conn);
         
         <input class='productID' type="hidden" hidden value="<?=$product->id?>">
     </div>
+    
     <input type="hidden" class="rowId" value="<?=$product->id?>">
 </div>
