831cd036c34d4afb914ce584760103f140227475
alex <Home@192.168.0.229>
2015-11-21 19:06:27 +0200

    Fix Саша

--- view/main.php
+++ view/main.php
@@ -8,6 +8,16 @@ defined(ACCESS_VALUE) or die('Access denied');
             <div>
                 <?php
                     $article = new Article();
+                    $article->getFomDb(array('id'=>'4'), $conn);
+                ?>
+                <a href="article-4-<?=$article->translit?>">
+                    <p><?=$article->title?></p>
+                    <img src="<?=$article->photo?>" alt="<?=$article->title?>"/>
+                </a>
+            </div>
+            <div>
+                <?php
+                    $article = new Article();
                     $article->getFomDb(array('id'=>'2'), $conn);
                 ?>
                 <a href="article-2-<?=$article->translit?>">
