e55cd813b1a9376de5b25335035a1f8180911619
zyma <zymainfo@gmail.com>
2016-07-22 17:53:55 +0300

    Fix Саша

--- view/blocks_site/header.php
+++ view/blocks_site/header.php
@@ -69,7 +69,7 @@ defined(ACCESS_VALUE) or die('Access denied');
                         <a href="actions"><span style="color:#FFD54F"><strong>Акции</strong></span></a>
                     </li>
                     <li>
-                        <a href="new-products"><strong>Новинки</strong></span></a>
+                        <a href="new-products"><span><strong>Новинки</strong></span></a>
                     </li>
                     <li>
                         <a href="info-1"><span>доставка и оплата</span></a>
